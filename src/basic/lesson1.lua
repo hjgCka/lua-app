fc5b612e37824beed3c2efe54d771b96431e1066
hjg <computer_cka@163.com>
2020-11-19 17:20:27 +0800

    添加lua文件和nginx默认配置文件。

---
+++ src/basic/lesson1.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: hjgCcka
+-- Date: 2020/11/19
+-- Time: 16:05
+-- To change this template use File | Settings | File Templates.
+--
+
+local a = 1
+local b = 2
+local c = "字符串"
+local d
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(print))
